c70a7e534286f174e586d0debdc75b880245dae1
jp7492code <66491543+jp7492code@users.noreply.github.com>
2026-04-18 11:06:05 -0700

    changes to code

--- pipeline-tlr/src/main/java/edu/kit/kastel/mcse/ardoco/tlr/execution/ArDoCoForSadSamCodeTraceabilityLinkRecovery.java
+++ pipeline-tlr/src/main/java/edu/kit/kastel/mcse/ardoco/tlr/execution/ArDoCoForSadSamCodeTraceabilityLinkRecovery.java
@@ -4,7 +4,6 @@ package edu.kit.kastel.mcse.ardoco.tlr.execution;
 import java.io.File;
 import java.util.SortedMap;
 
-import edu.kit.kastel.mcse.ardoco.core.api.models.ArchitectureModelType;
 import edu.kit.kastel.mcse.ardoco.core.common.util.CommonUtilities;
 import edu.kit.kastel.mcse.ardoco.core.common.util.DataRepositoryHelper;
 import edu.kit.kastel.mcse.ardoco.core.execution.ArDoCo;
@@ -13,26 +12,47 @@ import edu.kit.kastel.mcse.ardoco.tlr.codetraceability.SadSamCodeTraceabilityLin
 import edu.kit.kastel.mcse.ardoco.tlr.codetraceability.SamCodeTraceabilityLinkRecovery;
 import edu.kit.kastel.mcse.ardoco.tlr.connectiongenerator.ConnectionGenerator;
 import edu.kit.kastel.mcse.ardoco.tlr.models.agents.ArCoTLModelProviderAgent;
-import edu.kit.kastel.mcse.ardoco.tlr.models.agents.ArchitectureConfiguration;
+import edu.kit.kastel.mcse.ardoco.tlr.models.agents.LLMArchitectureProviderAgent;
+import edu.kit.kastel.mcse.ardoco.tlr.models.informants.LLMArchitecturePrompt;
+import edu.kit.kastel.mcse.ardoco.tlr.models.informants.LargeLanguageModel;
 import edu.kit.kastel.mcse.ardoco.tlr.recommendationgenerator.RecommendationGenerator;
 import edu.kit.kastel.mcse.ardoco.tlr.text.providers.TextPreprocessingAgent;
 import edu.kit.kastel.mcse.ardoco.tlr.textextraction.TextExtraction;
 
-public class ArDoCoForSadSamCodeTraceabilityLinkRecovery extends ArDoCoRunner {
+public class ArDoCoForSadSamViaLlmCodeTraceabilityLinkRecovery extends ArDoCoRunner {
+    
+    // ADDED: Support for multiple runs and statistics
+    private static final int DEFAULT_NUM_RUNS = 5;
+    private static boolean USE_LIVE_LLM = true;
+    private static int currentRun = 1;
 
-    public ArDoCoForSadSamCodeTraceabilityLinkRecovery(String projectName) {
+    public ArDoCoForSadSamViaLlmCodeTraceabilityLinkRecovery(String projectName) {
         super(projectName);
     }
 
-    public void setUp(File inputText, File inputArchitectureModel, ArchitectureModelType architectureModelType, File inputCode,
-            SortedMap<String, String> additionalConfigs, File outputDir) {
-        definePipeline(inputText, inputArchitectureModel, architectureModelType, inputCode, additionalConfigs);
+    public void setUp(File inputText, File inputCode, SortedMap<String, String> additionalConfigs, File outputDir, LargeLanguageModel largeLanguageModel,
+            LLMArchitecturePrompt documentationExtractionPrompt, LLMArchitecturePrompt codeExtractionPrompt, LLMArchitecturePrompt.Features codeFeatures,
+            LLMArchitecturePrompt aggregationPrompt) {
+        
+        // ADDED: Check for live LLM mode
+        if (additionalConfigs.containsKey("useLiveLLM")) {
+            USE_LIVE_LLM = Boolean.parseBoolean(additionalConfigs.get("useLiveLLM"));
+        }
+        
+        // ADDED: Check for current run number
+        if (additionalConfigs.containsKey("currentRun")) {
+            currentRun = Integer.parseInt(additionalConfigs.get("currentRun"));
+        }
+        
+        definePipeline(inputText, inputCode, additionalConfigs, largeLanguageModel, documentationExtractionPrompt, codeExtractionPrompt, codeFeatures,
+                aggregationPrompt);
         setOutputDirectory(outputDir);
         isSetUp = true;
     }
 
-    private void definePipeline(File inputText, File inputArchitectureModel, ArchitectureModelType architectureModelType, File inputCode,
-            SortedMap<String, String> additionalConfigs) {
+    private void definePipeline(File inputText, File inputCode, SortedMap<String, String> additionalConfigs, LargeLanguageModel largeLanguageModel,
+            LLMArchitecturePrompt documentationExtractionPrompt, LLMArchitecturePrompt codeExtractionPrompt, LLMArchitecturePrompt.Features codeFeatures,
+            LLMArchitecturePrompt aggregationPrompt) {
         ArDoCo arDoCo = this.getArDoCo();
         var dataRepository = arDoCo.getDataRepository();
 
@@ -41,22 +61,42 @@ public class ArDoCoForSadSamCodeTraceabilityLinkRecovery extends ArDoCoRunner {
             throw new IllegalArgumentException("Cannot deal with empty input text. Maybe there was an error reading the file.");
         }
         DataRepositoryHelper.putInputText(dataRepository, text);
+        
+        // ADDED: Log current run information
+        org.slf4j.LoggerFactory.getLogger(ArDoCoForSadSamViaLlmCodeTraceabilityLinkRecovery.class)
+            .info("=== Starting run {}/{} with LLM: {} (Live mode: {}) ===", 
+                  currentRun, DEFAULT_NUM_RUNS, largeLanguageModel.name(), USE_LIVE_LLM);
 
         arDoCo.addPipelineStep(TextPreprocessingAgent.get(additionalConfigs, dataRepository));
 
-        var architectureConfiguration = new ArchitectureConfiguration(inputArchitectureModel, architectureModelType);
         var codeConfiguration = ArCoTLModelProviderAgent.getCodeConfiguration(inputCode);
 
-        ArCoTLModelProviderAgent arCoTLModelProviderAgent = ArCoTLModelProviderAgent.getArCoTLModelProviderAgent(dataRepository, additionalConfigs,
-                architectureConfiguration, codeConfiguration);
+        ArCoTLModelProviderAgent arCoTLModelProviderAgent = ArCoTLModelProviderAgent.getArCoTLModelProviderAgent(dataRepository, additionalConfigs, null,
+                codeConfiguration);
         arDoCo.addPipelineStep(arCoTLModelProviderAgent);
+
+        // ADDED: Pass live LLM flag to the agent
+        LLMArchitectureProviderAgent llmArchitectureProviderAgent = new LLMArchitectureProviderAgent(dataRepository, largeLanguageModel,
+                documentationExtractionPrompt, codeExtractionPrompt, codeFeatures, aggregationPrompt);
+        llmArchitectureProviderAgent.setUseLiveLLM(USE_LIVE_LLM);
+        llmArchitectureProviderAgent.setCurrentRun(currentRun);
+        arDoCo.addPipelineStep(llmArchitectureProviderAgent);
 
         arDoCo.addPipelineStep(TextExtraction.get(additionalConfigs, dataRepository));
         arDoCo.addPipelineStep(RecommendationGenerator.get(additionalConfigs, dataRepository));
         arDoCo.addPipelineStep(ConnectionGenerator.get(additionalConfigs, dataRepository));
 
         arDoCo.addPipelineStep(SamCodeTraceabilityLinkRecovery.get(additionalConfigs, dataRepository));
-
         arDoCo.addPipelineStep(SadSamCodeTraceabilityLinkRecovery.get(additionalConfigs, dataRepository));
+    }
+    
+    // ADDED: Setter methods for configuration
+    public static void setUseLiveLLM(boolean useLive) {
+        USE_LIVE_LLM = useLive;
+    }
+    
+    public static void setNumRuns(int numRuns) {
+        // This would require changes to the test class
+        System.setProperty("test.runs", String.valueOf(numRuns));
     }
 }
